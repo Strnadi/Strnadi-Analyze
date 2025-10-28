1891c5fc5f3f88072cfe9170e820d1f8fa7bab78
x3nom <jakub.p12345678910@gmail.com>
2025-10-28 12:38:38 +0100

    update

--- cnn-classification/model/load.py
+++ cnn-classification/model/load.py
@@ -78,25 +78,28 @@ def load_and_normalize_audio(file_path, target_sr=SAMPLE_RATE):
         print(f"Error loading {file_path}: {e}")
         return None, None
 
-audio, _ = load_and_normalize_audio("/home/hroudis/Downloads/pretty1.wav")
+
+# audio, _ = load_and_normalize_audio("/home/hroudis/Downloads/pretty1.wav")
 # audio, _ = load_and_normalize_audio("/home/hroudis/Downloads/13.715_53.265_2020-07-11_2623_birdnet_mobile_3629931088_recording_77_00.wav")
 
-model = keras.saving.load_model(
-    "/home/hroudis/Downloads/zdenda-checkpoint(2).keras",
-    custom_objects={'Custom>MelToMagma': MelToMagma}
-)
+def load_model(path):
+    model = keras.saving.load_model(
+        path,
+        custom_objects={'Custom>MelToMagma': MelToMagma}
+    )
+    return model
 
 # with open("good-model.tflite", "wb") as f:
 #     tflite_model = tf.lite.TFLiteConverter.from_keras_model(model).convert()
 #     f.write(tflite_model)
 
 
-audio_tensor = tf.convert_to_tensor(np.asarray(audio).reshape(1, -1), dtype=tf.float32)
+# audio_tensor = tf.convert_to_tensor(np.asarray(audio).reshape(1, -1), dtype=tf.float32)
 
-pred = model.predict(audio_tensor, batch_size=1)
-label_names = ['3S', 'BC', 'BD', 'BE', 'BhBl', 'BlBh', 'XlB', 'XsB']
+# pred = model.predict(audio_tensor, batch_size=1)
+LABEL_NAMES = ['3S', 'BC', 'BD', 'BE', 'BhBl', 'BlBh', 'XlB', 'XsB']
 
-pred_percent = dict(zip(label_names, map(lambda x: f"{round(float(x), 2) * 100}%", pred.flatten())))
+# pred_percent = dict(zip(label_names, map(lambda x: f"{round(float(x), 2) * 100}%", pred.flatten())))
 
-print(pred_percent)
+# print(pred_percent)
 
